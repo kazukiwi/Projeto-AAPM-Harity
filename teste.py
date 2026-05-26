a3c134e3e654cf9192632c923a23ee2ed2197e03
kazuki <kazukishiroma06@gmail.com>
2026-05-26 10:52:55 -0300

    atualiza variáveis de ambiente e implementa função para criar usuários no banco de dados

--- teste.py
+++ teste.py
@@ -1,3 +1,9 @@
-from app.database import DATABASE_URL
+from app.auth import ACCESS_TOKEN_EXPIRACAO_MINUTOS
+from datetime import datetime, timedelta, timezone
 
-print(DATABASE_URL)
+print(type(ACCESS_TOKEN_EXPIRACAO_MINUTOS))
+
+print(type(int(ACCESS_TOKEN_EXPIRACAO_MINUTOS)))
+
+expira = datetime.now(timezone.utc) + timedelta(minutes=int(ACCESS_TOKEN_EXPIRACAO_MINUTOS))
+print(type(expira))
